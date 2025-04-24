fb0fa0e1f26487a1c4fd8c4167edc9a485d83046
woo_ooni <heeey@nate.com>
2025-04-24 15:46:33 +0900

    feat : 상수 클래스 생성

---
+++ src/main/java/com/limited/product/common/Constants.java
@@ -0,0 +1,15 @@
+package com.limited.product.common;
+
+public class Constants {
+    public static final String SUCCESS = "success";
+
+    public static final String MEMBER_NOT_FOUND_BY_ID = "해당 아이디의 회원이 존재하지 않습니다.";
+    public static final String ALREADY_REGISTERED_MEMBER = "이미 가입된 회원입니다.";
+    public static final String NOT_ALLOWED_TO_ACCESS = "접근 가능한 유저가 아닙니다.";
+    public static final String CONNECTED_SUCCESSFULLY = "접속되었습니다.";
+    public static final String USER_ADDED_TO_QUEUE = "대기열에 등록되었습니다.";
+
+    public static final String EXCEEDS_MAX_PURCHASE_QUANTITY = "최대 구매 수량을 초과했습니다. 최대구매 가능 수량 : ";
+    public static final String INSUFFICIENT_STOCK = "현재 남은 재고가 없습니다.";
+    public static final String PRODUCT_NOT_FOUND = "해당 상품이 존재하지 않습니다.";
+}
