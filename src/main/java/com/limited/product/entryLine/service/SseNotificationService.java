9b7437dd5e87982bcfd4a03f334801f1ff391002
woo_ooni <heeey@nate.com>
2025-04-24 15:50:08 +0900

    chore: 불필요한 주석 제거

--- src/main/java/com/limited/product/entryLine/service/SseNotificationService.java
+++ src/main/java/com/limited/product/entryLine/service/SseNotificationService.java
@@ -10,35 +10,30 @@ import java.util.concurrent.ConcurrentHashMap;
 @Service
 public class SseNotificationService {
     private final Map<String, SseEmitter> emitters = new ConcurrentHashMap<>();
+    private static final String EVENT_NAME = "waiting-number";
 
     public SseEmitter subscribe(String userId) {
         SseEmitter emitter = new SseEmitter(1000L * 60 * 60);
 
-        // userId에 emitter 저장
         emitters.put(userId, emitter);
 
-        // 연결 종료시 emitter 제거
         emitter.onCompletion(() -> emitters.remove(userId));
 
-        // 타임아웃 발생시 emitter 제거
         emitter.onTimeout(() -> emitters.remove(userId));
 
-        // 오류 발생시 emitter 제거
         emitter.onError(e -> emitters.remove(userId));
 
         return emitter;
     }
 
     public void send(String userId, String message) {
-        // 해당 유저 emitter 가져오기
         SseEmitter emitter = emitters.get(userId);
 
-        // emitter가 존재하면 메세지 전송
         if (emitter != null) {
             try {
                 emitter.send(
                         SseEmitter.event()
-                                .name("waiting-number")
+                                .name(EVENT_NAME)
                                 .data(message)
                 );
             } catch (IOException e) {
